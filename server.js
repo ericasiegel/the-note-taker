392b711922f566a3b242c2c3d538bc33bb13ba1a
Erica Siegel <siegel.erica@gmail.com>
2021-01-09 23:58:53 -0800

    added post request and validation

--- server.js
+++ server.js
@@ -1,30 +1,86 @@
-//require data from db.json
+const fs = require('fs');
+const path = require('path');
+
 const notes = require('./data/db.json');
 
 const express = require('express');
 
+// port variable for heroku and local host
 const PORT = process.env.PORT || 3001;
 
 // instantiate the express server
 const app = express();
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+// function to find the specific note by it's id number
 function findById(id, notesArray) {
     const result = notesArray.filter(note => note.id === id)[0];
     return result;
 }
 
+// function to create new notes
+function createNewNote(body, notesArray) {
+    const note = body;
+    // push new note to the notes array in the json file
+    notesArray.push(note);
+
+    // write the new note to the json file
+    fs.writeFileSync(
+        path.join(__dirname, './data/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    
+    // return finished code to poste route for response
+    return note;
+}
+
+// function to validate text is entered correctly
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+      return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+      return false;
+    }
+
+    return true;
+  }
+
+// get request for the notes array
 app.get('/api/notes', (req, res) => {
     res.json(notes);
     // console.log(notes);
 });
 
+// get request to get the note by it's id parameter
 app.get('/api/notes/:id', (req, res) => {
     const result = findById(req.params.id, notes);
     if (result) {
         res.json(result);
-    } else {
+    } else { // returned error if there isn't a requested id number
         res.sendStatus(404);
     }
+});
+
+// post request to add notes
+app.post('/api/notes', (req, res) => {
+    // set the id of the note by generating a random number, base of 36 characters and grab the first 4 characters
+    req.body.id = Math.random().toString(36).substr(2, 4);
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateNote(req.body)) {
+        res.status(400).send('The Note is not propery formatted!');
+    } else {
+        //add the note to the json file
+        const note = createNewNote(req.body, notes);
+
+        res.json(req.body);
+    }
+
 });
 
 app.listen(PORT, () => {
