c664e53631ace466aa48426cd901a006cea42d7d
Erica Siegel <siegel.erica@gmail.com>
2021-01-09 23:08:53 -0800

    added id parameter

--- server.js
+++ server.js
@@ -8,17 +8,24 @@ const PORT = process.env.PORT || 3001;
 // instantiate the express server
 const app = express();
 
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+}
+
 app.get('/api/notes', (req, res) => {
     res.json(notes);
     // console.log(notes);
 });
 
-
-app.get('/api/notes', (req, res) => {
-    let results = notes;
-    console.log(req.query)
-    res.json(results);
-})
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.sendStatus(404);
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port 3001!`);
